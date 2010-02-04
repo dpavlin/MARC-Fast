798e5a294ff67a1ff4dbd8b7ecfb97020929daa4
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-02-04 17:15:09 +0000

    bump version [0.10]
    git-svn-id: svn+ssh://llin/home/dpavlin/private/svn/MARC-Fast/trunk@35 49f9634a-d7ec-0310-8e6b-ec35c6cc8804

--- Fast.pm
+++ Fast.pm
@@ -7,7 +7,7 @@ use Data::Dump qw/dump/;
 BEGIN {
 	use Exporter ();
 	use vars qw ($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-	$VERSION     = 0.09;
+	$VERSION     = 0.10;
 	@ISA         = qw (Exporter);
 	#Give a hoot don't pollute, do not export more than needed by default
 	@EXPORT      = qw ();
